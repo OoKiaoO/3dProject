99c8a08334956d4cbe57558355cf4cc83f0c5318
Chiara Campalani <campalanichiara@gmail.com>
2023-05-30 19:55:01 +0900

    added WebgiViewer component

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -18,7 +18,7 @@ function Jumbotron() {
             <h2 className="title">New</h2>
             <img className='logo' src={Iphone} alt="iPhone 14 Pro" />
             <p className='text'>Big and bigger.</p>
-            <span classname='description'>
+            <span className='description'>
                 From $41.62/mo. for 24 mo. or $999 before trade-in
             </span>
             <ul className='links'>
